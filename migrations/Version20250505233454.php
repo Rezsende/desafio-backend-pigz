40f2480da53e90b35cc7c1b123bacad480eb309c
João Paulo <jpresendejava@gmail.com>
2025-05-05 21:31:13 -0300

    Feat: dockerizando a aplicação, ajustando o README.md

---
+++ migrations/Version20250505233454.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250505233454 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, lista_id INT NOT NULL, descricao VARCHAR(255) NOT NULL, concluida TINYINT(1) NOT NULL, INDEX IDX_1F1B251E6736D68F (lista_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE lista (id INT AUTO_INCREMENT NOT NULL, usuario_id INT NOT NULL, nome VARCHAR(255) NOT NULL, INDEX IDX_FB9FEEEDDB38439E (usuario_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(100) DEFAULT NULL, senha VARCHAR(255) NOT NULL, nivel_acesso VARCHAR(50) NOT NULL, data_criacao DATETIME NOT NULL, data_atualizacao DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE item ADD CONSTRAINT FK_1F1B251E6736D68F FOREIGN KEY (lista_id) REFERENCES lista (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE lista ADD CONSTRAINT FK_FB9FEEEDDB38439E FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE item DROP FOREIGN KEY FK_1F1B251E6736D68F
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE lista DROP FOREIGN KEY FK_FB9FEEEDDB38439E
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE item
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE lista
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE usuario
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
+        SQL);
+    }
+}
